587092965564356a08f2afa04df3b8bb3133e053
cake-oh <keiko.nomuraa@gmail.com>
2024-03-13 08:48:22 -0700

    initialize site

---
+++ js/script.js
@@ -0,0 +1,295 @@
+//////// fishing diversity map ////////
+
+//// establish map variable ////
+var map2 = L.map('map2').setView([0, 200], 2.5); // Adjust the view and zoom level
+
+
+
+//// add base layer w/ bathymetry and coastlines ////
+L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}).addTo(map2);
+
+
+
+//// fetch country boundaries ////
+// fetch('data/world-administrative-boundaries.json')
+//     .then(function(response) {
+//         return response.json(); // Convert the response to JSON
+//     })
+//     .then(function(geojsonData) {
+//         // Now that we have the GeoJSON data, we can use it here
+//         L.geoJSON(geojsonData).addTo(map2);
+//     })
+//     .catch(function(error) {
+//         // It's good practice to catch and handle any errors
+//         console.error('error:', error);
+//     });
+
+
+
+//// fetch EEZ boundaries (national waters) ////
+var eezLayers = {}; // make container for EEZ layer
+var eezGeoJSONLayer;
+
+fetch('data/eez_boundaries_v12_0_360.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(geojsonData) {
+        // filter out unwanted EEZ attributes
+        geojsonData.features = geojsonData.features.filter(function(feature) {
+            return feature.properties.LINE_TYPE !== "Archipelagic baseline" &&
+                    feature.properties.LINE_TYPE !== "Straight baseline";
+        });
+        // style map
+        L.geoJSON(geojsonData, {
+            style: function(feature) { 
+                return {
+                    color: "darkblue", 
+                    weight: 1.5,        
+                    dashArray: '4',
+                    opacity: .6    
+                };
+            },
+        // group EEZ for pop-up
+            onEachFeature: function(feature, layer) {
+                var eez1 = feature.properties.EEZ1.replace(/\(.*?\)/g, "").trim(); // grab EEZ name - get rid of parentheses 
+                var territory1 = feature.properties.TERRITORY1; // grab country it belongs to name
+
+                if(!eezLayers[territory1]){
+                    eezLayers[territory1] = [];
+                }
+                eezLayers[territory1].push(layer);
+
+                layer.bindPopup("<strong>" + territory1 + "</strong>" + "</br>" + eez1); // fill pop-up w/ EEZ and country
+        // make pop-up on mouseover: thicken line when mouse is on
+                layer.on({
+                    mouseover: function(e) {
+                        eezLayers[territory1].forEach(function(layer) {
+                            var layer = e.target;
+                            layer.setStyle({
+                                weight: 15 // Set the weight to highlight
+                            });
+                        });
+                        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                            layer.bringToFront();
+                        }
+                        infoControl.update(layer.feature.properties); // Update the control with properties of the hovered feature
+                    },
+                    mouseout: function(e) {
+                        eezLayers[territory1].forEach(function(layer) {
+                            layer.setStyle({
+                                weight: 1.5 // Reset to the original weight
+                            });
+                        });
+                        infoControl.update(null);
+                    }
+                });
+            },
+        }).addTo(map2);
+    })
+    .catch(function(error) {
+        console.error('error:', error);
+    });
+
+// function for the mouseovers
+var infoControl = L.control({position: 'topright'}); 
+
+infoControl.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update(); 
+    return this._div;
+};
+
+infoControl.update = function(props) {
+    if (props) { // if on an EEZ, show its information
+        var eez1 = props.EEZ1.replace(/\(.*?\)/g, "").trim(); // grab EEZ name - get rid of parentheses 
+        var territory1 = props.TERRITORY1; // grab country it belongs to name
+        this._div.innerHTML = '<h4>EEZ Information</h4>' + 
+            '<b>' + territory1 + '</b><br />' + eez1;
+    } else { // else, just show instructions
+        this._div.innerHTML = 'Hover over an EEZ';
+    }
+};
+infoControl.addTo(map2);
+
+
+
+
+
+//// add the raster datasets ////
+// diversity
+var imageBounds = [[-47,102], [50, 271]]; // note: keep these the same as the raster extents in R
+
+var fishing = L.imageOverlay('data/pacific_ocean_raster_div.png', imageBounds, { // fishing diversity
+    opacity: 0.1,
+    interactive: true
+}).addTo(map2);
+
+// abundance
+var imageBounds = [[-47,102], [50, 271]];
+
+var conflict = L.imageOverlay('data/pacific_ocean_raster_abundance.png', imageBounds, { // conflict hotspots
+    opacity: 0.5,
+    interactive: true,
+
+}).addTo(map2);
+
+
+
+//// blender for layer 1: diversity ////
+var opacityControldiv = L.control({position: 'bottomleft'});
+opacityControldiv.onAdd = function(map) {
+    var div = L.DomUtil.create('div');
+    div.innerHTML = '<div style="text-align: center; margin-bottom: 5px;">Fishing Diversity</div>' +
+    '<input type="range" id="opacitySliderdiv" min="0" max="1" step="0.1" value="0.1" style="width:200px;">';
+    L.DomEvent.disableClickPropagation(div);
+    return div;
+};
+opacityControldiv.addTo(map2);
+
+// event listener for the slider
+document.getElementById('opacitySliderdiv').addEventListener('input', function(e) {
+    var newOpacity = e.target.value;
+    fishing.setOpacity(newOpacity); // Adjust the opacity of the second raster based on the slider
+});
+
+
+
+//// blender for layer 2: conflict ////
+var opacityControlcon = L.control({position: 'bottomright'});
+opacityControlcon.onAdd = function(map) {
+    var div = L.DomUtil.create('div');
+    div.innerHTML = '<div style="text-align: center; margin-bottom: 5px;">Fishing Effort</div>' +
+    '<input type="range" id="opacitySlidercon" min="0" max="1" step="0.1" value="0.5" style="width:200px;">';    L.DomEvent.disableClickPropagation(div);
+    return div;
+};
+opacityControlcon.addTo(map2);
+
+// event listener for the slider
+document.getElementById('opacitySlidercon').addEventListener('input', function(e) {
+    var newOpacity = e.target.value;
+    conflict.setOpacity(newOpacity); // Adjust the opacity of the second raster based on the slider
+});
+
+
+
+//// load json data to call for diversity and conflict value pop-up ////
+// diversity data
+let diversityData = []; // make container
+fetch('data/data_diversity_longline.json')
+    .then(response => response.json())
+    .then(data => {
+        diversityData = data;
+    })
+    .catch(error => console.error('error:', error));
+
+// conflict data 
+var abundanceData = []; // Make sure this is accessible
+
+fetch('data/data_diversity_longline_webmap.json')
+    .then(response => response.json())
+    .then(data => {
+        abundanceData = data; // Assuming this data is an array of objects
+    })
+    .catch(error => console.error('error:', error));
+
+// create event listener
+map2.on('click', function(e) {
+    const lat = e.latlng.lat;
+    const lng = e.latlng.lng;
+
+    let popupContent = '';
+
+    // find the closest conflict data point
+    if (abundanceData.length > 0) {
+        const closestabundanceData = abundanceData.reduce((prev, curr) => {
+            const prevDist = Math.sqrt(Math.pow(prev.lat - lat, 2) + Math.pow(prev.lon - lng, 2));
+            const currDist = Math.sqrt(Math.pow(curr.lat - lat, 2) + Math.pow(curr.lon - lng, 2));
+            return (prevDist < currDist) ? prev : curr;
+        });
+        popupContent += `<strong>Fishing Intensity: </strong>${closestabundanceData.total_fe.toFixed(2)}<br>`;
+
+    // find the closest diversity data point
+    if (diversityData.length > 0) {
+        const closestDiversityData = diversityData.reduce((prev, curr) => {
+            const prevDist = Math.sqrt(Math.pow(prev.lat - lat, 2) + Math.pow(prev.lon - lng, 2));
+            const currDist = Math.sqrt(Math.pow(curr.lat - lat, 2) + Math.pow(curr.lon - lng, 2));
+            return (prevDist < currDist) ? prev : curr;
+        });
+        popupContent += `<strong>International Diversity: </strong>${closestDiversityData.fe_diversity_norm.toFixed(2)}`;
+    } 
+    }
+
+    // display popup
+    if (popupContent !== '') {
+        L.popup()
+            .setLatLng(e.latlng)
+            .setContent(popupContent)
+            .openOn(map2);
+    }
+});
+
+
+
+
+
+
+//// add legends ////
+// establish color palette
+var spectralColors = ["#9E0142","#D53E4F", "#F46D43", "#FDAE61", "#FEE08B", "#FFFFBF", "#E6F598", "#ABDDA4","#66C2A5", "#3288BD", "#5E4FA2"];
+var redylColors = ["#FFFFFF" ,"#F0F0F0", "#D9D9D9", "#BDBDBD", "#969696", "#737373", "#525252", "#252525","#000000"];
+
+// add to map
+var diversityLegend = createLegend(spectralColors.reverse(), "Fishing Diversity",0,1.83,'bottomleft');
+diversityLegend.addTo(map2);
+
+var abundanceLegend = createLegend(redylColors, "Fishing Effort<br>(logged hours)",0,11,'bottomright');
+abundanceLegend.addTo(map2);
+
+// build function for legend
+function createLegend(colors, title, minValue, maxValue, position) {
+    var legend = L.control({position: position}); // Use the position parameter
+
+    legend.onAdd = function(map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            labels = [],
+            range = maxValue - minValue,
+            segment = range / (colors.length - 1);
+
+        div.innerHTML += '<strong>' + title + '</strong><br>';
+
+        // Generate labels with value ranges
+        for (var i = 0; i < colors.length; i++) {
+            var fromValue = minValue + (i * segment);
+            var toValue = fromValue + segment;
+            
+            // Formatting the label text
+            var labelText = i < colors.length - 1 ?
+                fromValue.toFixed(2) + " - " + toValue.toFixed(2) :
+                "> " + fromValue.toFixed(2);
+
+            labels.push(
+                '<i style="background:' + colors[i] + '"></i> ' + labelText);
+        }
+
+        div.innerHTML += labels.join('<br>');
+        return div;
+    };
+
+    return legend;
+}
+
+
+
+
+
+//// establish functions (from lab 3, maybe useful) ////
+/// number formatting
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
---
+++ prep_rasters.R
@@ -0,0 +1,169 @@
+
+## successful go 
+library(raster)
+library(rgdal)
+library(RColorBrewer)
+
+# Load the CSV data - PICK DIVERSITY OR CONFLICT
+csv_data <- read.csv("output/data_diversity_longline_webmap.csv")
+# csv_data <- read.csv("output/data_conflict_longline_webmap.csv")
+
+
+# Create a raster from the CSV data
+# diversity, abundance
+# r <- rasterFromXYZ(csv_data[, c("lon", "lat", "fe_diversity_norm")], crs = CRS("+proj=longlat"))
+csv_data$total_fe <- log(csv_data$total_fe+1)
+r <- rasterFromXYZ(csv_data[, c("lon", "lat", "total_fe")], crs = CRS("+proj=longlat"))
+# conflict
+# r <- rasterFromXYZ(csv_data[, c("lon", "lat", "n_dyads")], crs = CRS("+proj=longlat"))
+
+
+# Set the extent based on the desired geographic bounds
+# extent(r) <- c(110, 270, -47, 50)  # Update with your data's actual extent if different
+r_merc <- r
+# Reproject the raster to Web Mercator
+# r_merc <- projectRaster(r, crs=CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"))
+
+# Calculate aspect ratio
+x_range <- abs(extent(r_merc)[1] - extent(r_merc)[2])
+y_range <- abs(extent(r_merc)[3] - extent(r_merc)[4])
+asp_ratio <- x_range / y_range
+
+# Define PNG dimensions
+png_width <- 2500  # Or another suitable width based on your needs
+png_height <- png_width / asp_ratio
+
+# Save the filled raster as a PNG file
+# png("/Users/keikonomura/Library/Mobile Documents/com~apple~CloudDocs/PhD stuff/webmapping_project/data/pacific_ocean_raster_div.png",
+# width=png_width, height=png_height,bg="transparent")
+png("/Users/keikonomura/Library/Mobile Documents/com~apple~CloudDocs/PhD stuff/webmapping_project/data/pacific_ocean_raster_abundance.png",
+    width=png_width, height=png_height,bg="transparent")
+# png("/Users/keikonomura/Library/Mobile Documents/com~apple~CloudDocs/PhD stuff/webmapping_project/data/conflict_raster.png",
+    # width=png_width, height=png_height,bg="transparent")
+par(mar=c(0,0,0,0), xaxs='i', yaxs='i')
+# plot(r_merc, axes=FALSE,bty="n", box=FALSE, col = rev(brewer.pal(11,"Spectral")),asp=1, legend=FALSE,useRaster=TRUE)  # Set useRaster=TRUE for large rasters
+plot(r_merc, axes=FALSE, bty="n", box=FALSE, col = rev(brewer.pal(11,"Greys")),asp=1, legend=FALSE, useRaster=TRUE)
+dev.off()
+
+#
+
+
+
+
+
+
+
+
+# for leaflet extents
+# Get the extent of the raster
+r_extent <- extent(r_filled)
+
+# Print the extent (optional)
+print(r_extent)
+
+
+# Load the raster image
+r <- raster("output.png")  # Replace with the path to your image
+
+
+projection(r) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+r <- projectRaster(r, crs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+
+# Save the filled raster as a PNG file
+pngfile <- "output.png"
+png(file = pngfile)
+
+# Set the plot margins to zero
+par(mar = rep(0, 4))
+
+# Plot the filled raster without a border
+plot(r, legend = FALSE, mar=c(0,0,0,0), axes = FALSE, box = FALSE, main = "",asp=1)
+
+
+dev.off()
+
+
+
+# Get the bounds of the raster image
+imageBounds <- extent(r)
+
+# Convert the bounds to a matrix for Leaflet
+imageBoundsMatrix <- matrix(c(imageBounds@xmin, imageBounds@ymin, imageBounds@xmax, imageBounds@ymax), ncol = 2)
+
+
+
+
+#### previous tries ####
+library(raster)
+
+# Read CSV data into a data frame
+df <- read.csv("output/data_diversity_longline.csv")
+
+# Convert the data frame to a raster
+r <- rasterFromXYZ(df[, c("lon", "lat", "fe_diversity_norm")])
+
+# Fill missing values with '0'
+r_filled <- reclassify(r, matrix(c(NA, NA, 0), ncol = 3))
+
+# Reproject the raster
+projection(r_filled) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+r_filled <- projectRaster(r_filled, crs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+
+# Save the filled raster as a PNG file
+pngfile <- "output.png"
+png(file = pngfile)
+
+# Set the plot margins to zero
+par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
+
+# Plot the raster image
+plot.new()
+# rasterImage(r_filled, 95, 276, -52,55)
+plot(r_filled, legend = FALSE, axes = FALSE, box = FALSE, main = "",asp=1)
+dev.off()
+
+
+
+
+
+## first go at it ##
+
+library(raster)
+library(rgdal) # For CRS and projectRaster
+library(rasterVis) # for advanced visualization
+
+
+# Read CSV data into a data frame
+df <- read.csv("output/data_diversity_longline.csv")
+
+# Convert the data frame to a raster
+r <- rasterFromXYZ(df[, c("lon", "lat", "fe_diversity_norm")], crs="+proj=longlat +datum=WGS84")
+
+# projection(r) <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+# r <- projectRaster(r, crs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+
+
+# Fill missing values with '0'
+r_filled <- reclassify(r, matrix(c(NA, NA, 0), ncol = 3))
+
+
+
+# It's important to ensure the raster covers the exact area you need, without margins.
+# Here, adjust extent if necessary to match your specific geographic bounds
+extent(r_filled) <- extent(c(-60,110,60,270)) # Set your actual bounds
+
+# Extract the min and max values for longitude and latitude from the extent
+xmin <- extent(r_filled)@xmin
+xmax <- extent(r_filled)@xmax
+ymin <- extent(r_filled)@ymin
+ymax <- extent(r_filled)@ymax
+
+# Define a suitable color palette for your data
+colorPalette <- terrain.colors(256)  # Feel free to choose another palette
+
+# Plot the raster using the defined color palette
+png(filename = "output.png")
+par(mar=c(0,0,0,0), oma=c(0,0,0,0), xaxs="i", yaxs="i")  # Set margins to zero and adjust axis intervals
+plot(r_filled, col=colorPalette, axes=FALSE, box=FALSE, ann=FALSE, legend=FALSE,
+     xlim=c(xmin, xmax), ylim=c(ymin, ymax))
+dev.off()
