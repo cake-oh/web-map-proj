87da29941583223718f52542515fb2d2b5287459
cake-oh <keiko.nomuraa@gmail.com>
2024-03-21 12:38:06 -0700

    up

--- js/script.js
+++ js/script.js
@@ -6,11 +6,10 @@ var map2 = L.map('map2').setView([0, 200], 2.5); // Adjust the view and zoom lev
 
 
 //// add base layer w/ bathymetry and coastlines ////
-L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
 }).addTo(map2);
 
 
